3f1ba978d4ed0024a8eb647efe5e391921f0ead3
samuelmarina <samuellmarina@gmail.com>
2020-04-15 02:17:33 -0400

    Added title

---
+++ app/src/main/java/com/samuelm/quehacerencuarentena/AddPhraseActivity.kt
@@ -0,0 +1,12 @@
+package com.samuelm.quehacerencuarentena
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPhraseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_phrase)
+    }
+}
